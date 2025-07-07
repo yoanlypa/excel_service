f953616523806df19e92a0ec900988d3971953e2
Yoanly Perez <98776473+yoanlypa@users.noreply.github.com>
2025-07-07 13:15:48 +0200

    Update excel_bot.py

--- excel_bot.py
+++ excel_bot.py
@@ -98,8 +98,9 @@ async def handle_doc(update, context):
 
     # Descarga a fichero temporal
     with tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False) as tmp:
-        await doc.get_file().download_to_drive(custom_path=tmp.name)
-        try:
+        tg_file = await doc.get_file()
+        await tg_file.download_to_drive(custom_path=tmp.name)
+         try:
             pedidos = parse_excel(tmp.name)
             r = requests.post(
                 DJANGO_URL,
